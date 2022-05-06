d87f6fa7f84cdf9cb2dad8f71b100d013d4b21b2
snipex111 <abhishekdltg25@gmail.com>
2022-05-06 12:09:42 +0530

    authentication

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const localstrategy = require('passport-local');
 const flash = require('connect-flash');
+const User = require('./models/users');
 
+
+const apperror = require('./apperror');
+const catchAsync = require('./catchAsync');
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
@@ -27,11 +31,79 @@ db.once("open", () => {
 })
 
 
+
+
+const session = require('express-session');
+
+const sessionOptions = {
+    secret: 'thisisnotagoodsecret', resave: false, saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 500000000,
+        maxAge: 500000000, httpOnly: true
+    }
+}
+app.use(flash());
+
+
+
+
+
+app.use(session(sessionOptions));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localstrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use((req, res, next) => {
+
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+
 app.get('/', (req, res) => {
     res.render('home');
 })
 
 
+app.get('/users/register', (req, res) => {
+    res.render('users/create')
+})
+
+app.get('/users/login', (req, res) => {
+    res.render('users/login')
+})
+app.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registereduser = await User.register(user, password);
+        req.login(registereduser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Health-E!');
+            res.redirect('/');
+        })
+    }
+    catch (err) {
+        req.flash('error', err.message);
+        res.redirect('/users/register');
+    }
+}))
+app.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/users/login' }), catchAsync(async (req, res) => {
+    try {
+        req.flash('success', 'welcome back!');
+
+        const redirectUrl = req.session.returnTo || '/';
+        delete req.session.returnTo;
+        res.redirect(redirectUrl);
+    }
+    catch (err) {
+        req.flash('error', 'Invalid Credentials');
+
+    }
+}))
 
 
 
@@ -44,6 +116,17 @@ app.get('/', (req, res) => {
 
 
 
+
+app.all('*', (req, res, next) => {
+    next(new apperror('page not found', 404));
+})
+
+app.use((err, req, res, next) => {
+    const { status1 = 500, } = err;
+    if (!err.message)
+        err.message = 'something went wrong';
+    res.status(status1).render('error', { err })
+})
 
 
 
---
+++ models/users.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const passportlocalmongoose = require('passport-local-mongoose');
+const UserSchema = new Schema({
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+
+});
+
+UserSchema.plugin(passportlocalmongoose);
+module.exports = mongoose.model('User', UserSchema);
